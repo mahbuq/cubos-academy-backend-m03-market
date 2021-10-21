4774aedd2f100202e664f1449c963345fbf68395
Mayara Carvalho <mahbuquerque@gmail.com>
2021-10-21 19:42:32 -0300

    update database

--- src/conexao.js
+++ src/conexao.js
@@ -1,11 +1,14 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-   user: "postgres",
-   host: "localhost",
-   database: "market_cubos",
-   password: "postgres",
+   user: "uiamupkehgbsae",
+   host: "ec2-52-21-193-223.compute-1.amazonaws.com",
+   database: "doh5e40i1aiq7",
+   password: "22bf07399e5019bd761f6664c64e18af3675a718bdce848650e972817d175e5b",
    port: 5432,
+   ssl: {
+      rejectUnauthorized: false,
+   },
 });
 
 const query = (text, param) => {
